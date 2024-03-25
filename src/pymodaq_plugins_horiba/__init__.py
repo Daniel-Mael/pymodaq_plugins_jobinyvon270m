b39fb328932fa219744c44b4240b3f2fc81952ed
Daniel-Mael <m_leblan@insa-toulouse.fr>
2024-03-25 17:17:31 +0100

    Update __init__.py
    Adding the configuration

--- src/pymodaq_plugins_horiba/__init__.py
+++ src/pymodaq_plugins_horiba/__init__.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 from pymodaq.utils.daq_utils import set_logger
 
+from .utils import Config
+config = Config()
+
 with open(str(Path(__file__).parent.joinpath('resources/VERSION')), 'r') as fvers:
     __version__ = fvers.read().strip()
